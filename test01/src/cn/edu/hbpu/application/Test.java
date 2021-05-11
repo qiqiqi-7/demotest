6a7b4c335e47fa2c09573dbaf0594ca2df82e636
duan <2981386671@qq.com>
2021-05-11 22:40:53 +0800

    test for demo

--- test01/src/cn/edu/hbpu/application/Test.java
+++ test01/src/cn/edu/hbpu/application/Test.java
@@ -7,5 +7,7 @@ public class Test {
     public static void main(String[] args) {
         Date date = new Date();
         System.out.println("duanyujie="+date);
+        System.out.println("demo for test");
+        System.out.println("dsadsadsadsads");
     }
 }
