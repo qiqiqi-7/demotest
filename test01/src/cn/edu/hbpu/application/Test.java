a0c505c9a3429db5a10f54cc98a984c768ff6d06
duan <2981386671@qq.com>
2021-05-11 22:35:49 +0800

    123

---
+++ test01/src/cn/edu/hbpu/application/Test.java
@@ -0,0 +1,11 @@
+package cn.edu.hbpu.application;
+
+import java.util.Date;
+
+public class Test {
+
+    public static void main(String[] args) {
+        Date date = new Date();
+        System.out.println("duanyujie="+date);
+    }
+}
